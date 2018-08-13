b254f20241aaef68af806cf0be8ef2cd81c69167
Javed Ahmadzai <theahmadzai@hotmail.com>
2018-08-13 12:09:03 +0500

    Fixed format

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,11 +2,15 @@ module.exports = {
   root: true,
   env: {
     es6: true,
-    node: true
+    node: true,
+    browser: true
   },
-  extends: [ 'eslint:recommended'],
+  extends: [ 'eslint:recommended' ],
   parserOptions: {
     parser: 'babel-eslint',
     sourceType: 'module'
+  },
+  rules: {
+    'no-console': 'off'
   }
 };
