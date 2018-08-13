ae36bfe80ad8d4454432c063ca826946b1059943
Javed Ahmadzai <theahmadzai@hotmail.com>
2018-08-13 12:09:12 +0500

    Test works now

--- test/index.js
+++ test/index.js
@@ -3,6 +3,19 @@
 const stylelint = require('stylelint');
 const rules = require('../src').rules;
 
-Object.keys(rules).forEach(rule=>{
-  stylelint.utils.validateOptions(rule);
+stylelint.lint({
+  code: 'a { color: pink; }',
+  config: {
+    rules: rules
+  }
+}).then(output => {
+  const { deprecations, invalidOptionWarnings, parseErrors, warnings } = output.results[0];
+  console.log(`Output:
+    Deprications: ${deprecations.length}
+    InvalidOptionWarnings: ${invalidOptionWarnings.length}
+    parseErrors: ${parseErrors.length}
+    Warnings: ${warnings.length}
+  `);
+}).catch(error => {
+  console.log(error);
 });
